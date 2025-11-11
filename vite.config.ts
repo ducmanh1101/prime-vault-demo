eecd137b40dfc56d69116743e7f801ae6b32f857
ducmanh1101 <jake@beraji.io>
2025-11-11 11:09:54 +0700

    fix key

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
-  // plugins: [react(), tailwindcss()],
-  plugins: [react(), tailwindcss(), mkcert()],
-  server: {
-    host: "myapp.local", // 👈 domain tùy chỉnh
-    port: 5173,
-    https: true,
-  },
+  plugins: [react(), tailwindcss()],
+  // plugins: [react(), tailwindcss(), mkcert()],
+  // server: {
+  //   host: "myapp.local", // 👈 domain tùy chỉnh
+  //   port: 5173,
+  //   https: true,
+  // },
 });
